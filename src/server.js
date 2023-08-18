65c0ed01734c80c706081c5bdd0ea8631224b346
rafifwhy <rafif.wahyu@student.umn.ac.id>
2023-08-19 00:17:42 +0700

    Change host address

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const Hapi = require('@hapi/hapi');
+
 const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {
             cors: {
               origin: ['*'],
@@ -15,6 +16,7 @@ const init = async () => {
     server.route(routes);
 
     await server.start();
+
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
